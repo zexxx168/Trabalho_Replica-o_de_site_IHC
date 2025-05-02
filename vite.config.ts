5e9e327389f375fce9ce100bed9cda74fbf7520b
brunoisaacshtavares <brunoisaacshtavares@gmail.com>
2025-05-01 22:23:25 -0300

    correção redirecionamento pagina inicial

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Trabalho_Replica-o_de_site_IHC/', 
+  base: '/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
